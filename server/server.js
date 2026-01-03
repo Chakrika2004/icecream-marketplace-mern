03cbabec21fa6f82067c9066bf217c662f6b98d2
Chakrika2004 <160034472+Chakrika2004@users.noreply.github.com>
2026-01-04 04:52:30 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", 'https://icecream-marketplace-mern.vercel.app'];
 
 app.post(
   "/api/stripe",
